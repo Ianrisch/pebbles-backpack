e540b04b39aaf8e627ee85fd117fb3d7416585c1
ThunfischEis <ian.risch@gmail.com>
2024-11-17 23:36:26 +0100

    Simplify ModComponents

--- src/main/kotlin/tech/sethi/pebbles/backpack/compenents/ModComponents.kt
+++ src/main/kotlin/tech/sethi/pebbles/backpack/compenents/ModComponents.kt
@@ -9,21 +9,17 @@ import net.minecraft.util.Uuids
 import tech.sethi.pebbles.backpack.PebblesBackpackInitializer
 import java.util.*
 
-class ModComponents {
+object ModComponents {
 
-    companion object {
-        val BackpackUUID: ComponentType<UUID> = Registry.register(
-            Registries.DATA_COMPONENT_TYPE,
-            Identifier.of(PebblesBackpackInitializer.MODID, "backpack_uuid"),
-            builder<UUID>().codec(Uuids.CODEC).build()
-        )
+    val BackpackUUID: ComponentType<UUID> = Registry.register(
+        Registries.DATA_COMPONENT_TYPE,
+        Identifier.of(PebblesBackpackInitializer.MODID, "backpack_uuid"),
+        builder<UUID>().codec(Uuids.CODEC).build()
+    )
 
-        fun initialize() {
-            PebblesBackpackInitializer.LOGGER.info("Registering {} components", PebblesBackpackInitializer.MODID)
-            // Technically this method can stay empty, but some developers like to notify
-            // the console, that certain parts of the mod have been successfully initialized
-        }
+    fun initialize() {
+        PebblesBackpackInitializer.LOGGER.info("Registering {} components", PebblesBackpackInitializer.MODID)
+        // Technically this method can stay empty, but some developers like to notify
+        // the console, that certain parts of the mod have been successfully initialized
     }
-
-
 }
